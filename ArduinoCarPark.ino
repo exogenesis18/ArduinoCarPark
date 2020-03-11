d1cc8c72a0c9f8f292564e99c5ad4aa23461fd2a
Aurora Bussola <aurorabussola@gmail.com>
2020-03-11 23:52:17 +0100

    Risolto bug auto-reset

--- ArduinoCarPark.ino
+++ ArduinoCarPark.ino
@@ -1,9 +1,6 @@
 //Corrispondono ai pin D1 e D2 del modulo wifi
-#define SS_PIN 4
-#define RST_PIN 5
 
 #include <SPI.h>
-#include <MFRC522.h>
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #include <LinkedList.h>
@@ -11,9 +8,7 @@
 #include <WiFiUdp.h>
 #include <SoftwareSerial.h>
 
-//SS permette la comunicazione via SPI, il secondo permette il reset
-MFRC522 mfrc522(SS_PIN, RST_PIN);
-SoftwareSerial s(D3, D4);
+SoftwareSerial s(D2, D3);
 
 #define FIREBASE_HOST "arduinocarpark-database.firebaseio.com"
 #define FIREBASE_AUTH "NKb6SKiqBLxTyzmbQBgEuycPckt28FBdlMJSv3hP"
@@ -26,19 +21,22 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 LinkedList<String> codici = LinkedList<String>();
-LinkedList<String> accessi = LinkedList<String>();
-String codici_effettuato_accesso[1000];
+String accessi[20];
+String codici_effettuato_accesso[20];
 int num_codici;
-int num_accessi;
 int indice;
 int indice_acc = 1;
+int i;
+String msg;
+unsigned long previousMillis = 0; //will store last time LED was updated
+unsigned long interval = 20000; //interval at which to blink (milliseconds)
+unsigned long currentMillis;
 
 /*                ***********************Setup*****************************             */
 void setup(){
   Serial.begin(115200);   //inizio la comunicazione seriale (mi serve per il monitor seriale)
   s.begin(9600);
   SPI.begin();      //inizio la comunicazione via SPI
-  mfrc522.PCD_Init();   //inizializzo il mio rfid reader
   
    
   //connessione al mio wifi
@@ -64,7 +62,11 @@ void setup(){
   Serial.println();
   Serial.println("Lista dei codici di utenti registrati:");
   while(1){
-    String stringa = Firebase.getString("ID clienti registrati/" + String(i) + "/codice");
+    String stringa;
+    do{
+      stringa = Firebase.getString("ID clienti registrati/" + String(i) + "/codice");
+    }
+    while(Firebase.failed());
     if (Firebase.success() && !stringa.equals("")){
       codici.add(stringa);
       Serial.println(codici.get(i-1));
@@ -85,34 +87,53 @@ void setup(){
 
 /*                ***********************Loop*****************************          */
 void loop(){
+
+  currentMillis = millis();
+  delay(10);
   
+  if(currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis; //save the last time you blinked the LED
+    //fa un controllo delle richieste di pagamento
+    for(i = 1; i <= indice_acc; i++){
+      if(Firebase.getString("ID clienti che hanno fatto accesso/" + String(i) + "/uscita/richiesta").equals("true")){
+        salvaOra("richiesta", i, true);
+        calcolaPrezzo(i);
+        Firebase.setString("ID clienti che hanno fatto accesso/" + String(i) + "/uscita/richiesta", "false");
+      }
+      delay(500);
+    }
+  }
   //controlla se viene appoggiato un badge
-  if ( ! mfrc522.PICC_IsNewCardPresent())
+  if(s.available() > 0){
+    while(s.available() > 0){
+      msg = msg + String(s.read());
+      delay(500);
+    }
+  }
+  else
     return;
-  //cerca di leggere il badge
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-    return;
-    
-  //viene letto il codice e mostrato su monitor
-  String content = leggiCodice();
+
+
+  String content = msg;
+  msg = "";
+  Serial.println(content);
 
   //controlla che il codice scannerizzato sia all'interno della lista dei codici che hanno già fatto accesso, se sì esce
-  for(int i = 0; i < num_accessi; i++){
-    if(accessi.get(i).equals(content.substring(1))){
+  for(int i = 0; i < indice_acc; i++){
+    if(accessi[i].equals(content) && codici_effettuato_accesso[i] != "ok"){
+      bool pagato;
+      do pagato = Firebase.getBool("ID clienti che hanno fatto accesso/" + String(i+1) + "/pagato");
+      while(Firebase.failed());
+      if(pagato == false){
+        Serial.println("Non hai pagato");
+        delay(5000);
+        return;
+      }
        s.write(2);
        Serial.println("Arrivederci :)");
-       accessi.remove(i);
-       num_accessi--;
-       int c = 0;
-       while(codici_effettuato_accesso){
-        if(codici_effettuato_accesso[c].equals(content.substring(1))){
-          codici_effettuato_accesso[c] = "ok";
-          salvaOra("uscita", c+1);
-          calcolaPrezzo(c+1);
-          break;
-        }
-        c++;
-       }
+       codici_effettuato_accesso[i] = "ok";
+       accessi[i] = "ok";
+       salvaOra("uscita", i+1, false);
 
        aggiornaPosti(false);
 
@@ -120,6 +141,8 @@ void loop(){
        return;
     }
   }
+
+  //controlla che ci siano posti disponibili
   int posti_tot;
   do posti_tot = Firebase.getInt("Posti/posti totali");
   while(Firebase.failed());
@@ -135,7 +158,7 @@ void loop(){
   //controlla che il codice scannerizzato sia all'interno della lista dei codici che ha il permesso di accedere
   bool esiste = false;
   for(int i = 0; i < num_codici; i++){
-    if(codici.get(i).equals(content.substring(1))){
+    if(codici.get(i).equals(content)){
        esiste = true;
        indice = i+1;
     }
@@ -149,19 +172,22 @@ void loop(){
   //se la stringa corrisponde ad una dei codici che hanno accesso, l'accesso viene accordato, altrimenti non viene accordato
   //stampa 1 se entra
   s.write(1);
-  accessi.add(content.substring(1));
-  num_accessi++;
+  accessi[indice_acc - 1] = content;
   aggiornaPosti(true);
     
   //stampa sul monitor
   Serial.println("Accesso consentito"); 
     
   //vengono mandati nel database i dati relativi all'accesso
-  String a = content.substring(1);
+  String a = content;
   do Firebase.setString("ID clienti che hanno fatto accesso/" + String(indice_acc) + "/codice", a);
   while(Firebase.failed());
-  salvaOra("arrivo", indice_acc);
-  codici_effettuato_accesso[indice_acc - 1] = content.substring(1);
+  salvaOra("arrivo", indice_acc, false);
+  codici_effettuato_accesso[indice_acc - 1] = content;
+  do Firebase.setBool("ID clienti che hanno fatto accesso/" + String(indice_acc) + "/pagato", false);
+  while(Firebase.failed());
+  do Firebase.setString("ID clienti che hanno fatto accesso/" + String(indice_acc) + "/uscita/richiesta", "false");
+  while(Firebase.failed());
   Serial.print("Ciao ");
   do Serial.print(Firebase.getString("ID clienti registrati/" + String(indice) + "/nome"));
   while(Firebase.failed());
@@ -184,38 +210,26 @@ void aggiornaPosti(bool entra){
   while(Firebase.failed());
 }
 
-void salvaOra(String stringa, int indice){
+void salvaOra(String stringa, int indice, bool richiesta){
   timeClient.update();
-  do Firebase.setInt("ID clienti che hanno fatto accesso/" + String(indice) + "/timestamp " + stringa, timeClient.getEpochTime());
-  while(Firebase.failed());
+  if(richiesta == false){
+    do Firebase.setInt("ID clienti che hanno fatto accesso/" + String(indice) + "/timestamp " + stringa, timeClient.getEpochTime());
+    while(Firebase.failed());
+  }
+  else{
+    do Firebase.setInt("ID clienti che hanno fatto accesso/" + String(indice) + "/uscita/timestamp " + stringa, timeClient.getEpochTime());
+    while(Firebase.failed());
+  }
   
 }
 
 void calcolaPrezzo(int indice){
   int secondiTot;
-  do secondiTot = Firebase.getInt("ID clienti che hanno fatto accesso/" + String(indice) + "/timestamp uscita") -
+  do secondiTot = Firebase.getInt("ID clienti che hanno fatto accesso/" + String(indice) + "/uscita/timestamp richiesta") -
   Firebase.getInt("ID clienti che hanno fatto accesso/" + String(indice) + "/timestamp arrivo");
   while(Firebase.failed());
 
-  do Firebase.setInt("ID clienti che hanno fatto accesso/" + String(indice) + "/prezzo", (secondiTot/60+1));
+  do Firebase.setInt("ID clienti che hanno fatto accesso/" + String(indice) + "/uscita/prezzo", (secondiTot/60+1));
   while(Firebase.failed());
   
-}
-
-String leggiCodice(){
-  Serial.println();
-  Serial.print("UID tag: ");
-  //viene costruita la stringa del codice
-  String content= "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  content.toUpperCase();
-  Serial.println();
-
-  return content;
 }
--- Arduino_MEGA/Arduino_MEGA.ino
+++ Arduino_MEGA/Arduino_MEGA.ino
@@ -1,11 +1,20 @@
 #include "SoftwareSerial.h"
+#include <RFID.h>
+#include <SPI.h>
+
+#define SS_PIN1 9
+#define RST_PIN 8
+
 
 SoftwareSerial s(10,11); //Rx, Tx
+RFID RC522(SS_PIN1, RST_PIN);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   s.begin(9600);
+  SPI.begin(); 
+  RC522.init();   //inizializzo il mio rfid reader
   
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -14,10 +23,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-   if(s.available() > 0){
-
-      //float val = ArduinoUno.parseFloat();
-
-      Serial.println(s.read());
+  if ( RC522.isCard()){
+   if ( RC522.readCardSerial()){
+    s.print(leggiCodice());
+    Serial.println(leggiCodice());
+    delay(15000);
    }
+  }
+}
+
+int leggiCodice(){
+  
+  //viene costruita la stringa del codice
+  int content = 0, c = 10000;
+  for(int i=0;i < 4;i++){
+    content+= RC522.serNum[i] * c;
+    c = c * 10000;
+  }
+
+  if(content < 0)
+    content = content * -1;
+    
+  return content;
 }
